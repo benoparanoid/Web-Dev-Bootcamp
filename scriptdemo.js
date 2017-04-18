63b44c5ae9a7849c0a508c42d2329c2c07544d11
beno <email1@yahoo.com>
2017-04-18 13:56:35 +0800

    JS First Exercise

---
+++ scriptdemo.js
@@ -0,0 +1,23 @@
+alert("Hello from the JS File");
+
+var userName = prompt("What is your Name");
+alert("Nice to meet you " + "userName");
+console.log("Also great to meet you " + "userName");
+
+
+
+// //alert
+// -pops up message
+// alert("Hello There")
+// alert(52 * 2345)
+
+// //console.log
+// -print something in the console
+// console.log("HEllo console")
+
+
+// //prompt
+// -lets user input
+// prompt("what is your name?")
+
+// var userName = prompt("What is your name");
